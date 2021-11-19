2a635e0a5d4a3f51c6d6618bf842fcd06d989b59
Mouizudiin <mouizuddin26@gmail.com>
2021-11-19 12:06:45 +0530

    easy

---
+++ easy/Find All Numbers Disappeared in an Array.py
@@ -0,0 +1,35 @@
+    # 448. Find All Numbers Disappeared in an Array
+
+
+'''
+Given an array nums of n integers where nums[i] is in the range [1, n], 
+return an array of all the integers in the range [1, n] that do not appear in nums.
+
+Example 1:
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+
+Example 2:
+Input: nums = [1,1]
+Output: [2] 
+'''
+
+# solution
+'''Note: 
+we also have duplicates in array (use a set() to remove duplicates)
+range is --->> [1,n] (key solution)
+ 1) set( range( 1,n+1) )
+ 2) for loop will give O(n) 
+'''
+
+def solution(array):
+    num_len = len(array)
+    the_range = set(range(1,num_len+1)) # [1,n]
+    for nums in array:
+        if nums in the_range:
+            the_range.remove(nums)
+    return list(the_range)
+
+
+testing = [1,1]
+print(solution(testing))
